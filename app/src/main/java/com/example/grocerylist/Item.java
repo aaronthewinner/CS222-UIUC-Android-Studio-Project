8b183036aad9514211849a5de91e6a40780ddc58
Aaron Taub <ajtaub2@illinois.edu>
2022-10-14 18:14:49 -0500

    realized my backend additions were not committed completely last week

--- app/src/main/java/com/example/grocerylist/Item.java
+++ app/src/main/java/com/example/grocerylist/Item.java
@@ -1,2 +1,16 @@
+package com.example.grocerylist;
+import java.util.Set;
 public class Item {
+    // name and id of item
+    private String name;
+    private long id;
+    // if we want users to efficiently search for things, we might want to put items into categories
+    private Set<String> categories;
+    // getters and setters for name and id(may not want)
+    public String getName(){ return name;}
+    public void setName(String newName) { name = newName;}
+    public long getId(){return id;}
+    public void SetId(long newId) { id = newId;}
+    // checks if an item falls into a particular category
+    public boolean isCategory(String search) { return categories.contains(search);}
 }
--- app/src/main/java/com/example/grocerylist/Store.java
+++ app/src/main/java/com/example/grocerylist/Store.java
@@ -1,16 +1,41 @@
 package com.example.grocerylist;
-import kotlin.collections.AbstractList;
-import Item;
+import java.util.List;
+
 public class Store {
     // name of store
     private String name;
     // id of store(if we have this)
     private long id;
     // list of items available at the store
-    private AbstractList<Item>;
+    private List<Item> items;
     // list of prices that always needs to align with the items
-    private AbstractList<double>;
+    // could have more than 1 item with the same price, so can't use sets here
+    private List<java.lang.Double> prices;
+    // getters and setters for name and id(may not want)
     public String getName(){ return name;}
     public void setName(String newName) { name = newName;}
     public long getId(){return id;}
+    public void setId(long newId) { id = newId;}
+    // checks if the store has a particular item
+    public boolean hasItem(String search) {
+        return itemSearch(search) != -1;
+    }
+    // helper method that searches for an item in the list
+    private int itemSearch(String search) {
+        for(int i = 0; i < items.size(); i++) {
+            if(items.get(i).getName() == search) {
+                return i;
+            }
+        }
+        // default value if item not found
+        return -1;
+    }
+    // gets the price of a specific item
+    public double getPrice(String search) {
+        if(itemSearch(search) == -1) {
+            // default value if item not found
+            return 0.0;
+        }
+        return prices.get(itemSearch(search));
+    }
 }
